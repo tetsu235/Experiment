ada6feaffcf85a8ed289ea456673c0c6ab09d6ab
tetsu235 <tyoshikawa2@student.cccd.edu>
2025-02-28 17:34:04 -0800

    Update sample_problems.cpp
    part of the solution

--- sample_problems.cpp
+++ sample_problems.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include<iomanip>
 
+int main()
+{
+  int num_blocks;
+  int weight_limit;
+  int temp_num_blocks;
+  int height_of_tower(0);
+  cout << "Enter the number of the blocks" << endl;
+  cin >> num_blocks;
+  cout << "enter the weight limit of the block" << ednl;
+  cin >> weight_limit;
+  do{
+    temp_num_blocks = num_blocks / (weight_limit + 1)
+  }while();
+  
+}
