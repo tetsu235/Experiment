a63eda56c4c115bd06e17102497064ec964d09a3
tetsu235 <tyoshikawa2@student.cccd.edu>
2025-02-28 19:37:01 -0800

    Update sample_problems.cpp

--- sample_problems.cpp
+++ sample_problems.cpp
@@ -5,14 +5,24 @@ int main()
 {
   int num_blocks;
   int weight_limit;
-  int temp_num_blocks;
   int height_of_tower(0);
   cout << "Enter the number of the blocks" << endl;
   cin >> num_blocks;
+  int temp_num_blocks(num_blocks);
   cout << "enter the weight limit of the block" << ednl;
   cin >> weight_limit;
-  do{
-    temp_num_blocks = num_blocks / (weight_limit + 1)
-  }while();
+  while(temp_num_blocks / (weight_limit + 1) <= temp_num_blocks)
+    {
+      if(temp_num_blocks % (weight_limit + 1) == 0)
+      {
+        temp_num_blocks -= temp_num_blocks / (weight_limit + 1);
+      }
+      else
+      {
+        temp_num_blocks -= temp_num_blocks / (weight_limit + 1) + 1;
+      }
+      height_of_tower++;
+    }
+  cout << "The highest tower floor is " << height_of_tower << endl;
   
 }
