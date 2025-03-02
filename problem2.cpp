cba887d5a5c2e7406c15282bfd000de76b80fb08
tetsu235 <tyoshikawa2@student.cccd.edu>
2025-03-02 14:27:58 -0800

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<iomanip>
 
+main()
+{
+  int num_of_station;
+  int num_of_stop;
+  int num_of_start;
+  int list_of_stop;
+  cout << "Enter the number of stations" << endl;
+  cin >> num_of_station;
+  cout << "Enter the number of stations you stop" << endl;
+  cin >> num_of_stop;
+  cout << "Enter the number of the station you start" << endl;
+  cin >> num_of_start;
+  do
+    {
+      cout << "Enter the number of the station you stop(0 to quit)" << endl;
+      cin >> num_of_stop;
+      
+    }
+  while(num_of_stop != 0);
+}
