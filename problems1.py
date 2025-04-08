7ba5b7241364affcd00779b377f951d92444e9c8
tetsu235 <tyoshikawa2@student.cccd.edu>
2025-04-08 16:41:24 -0700

    Update problems1.py

--- problems1.py
+++ problems1.py
@@ -31,6 +31,36 @@ for i in range(0, Q):
 if count == Q:
   print(-1)
 
+#9.1 
+def check(l):
+    result = true
+    for i in range(num - 1):
+      if l[i] > l[i + 1]:
+        result = false
+        break
+    return result
+    
+num_of_element = int(input("Enter the number of elements in series: "))
+series = list(map(int, input("Enter the numbers, separated by space: ").split()))
+num_of_order = int(input("Enter the number of the order: "))
+num_required = 0
+for i in range(num_of_order):
+  if check(series):
+    break
+  num_required++
+  order = tuple(map(int, input("Enter two numbers to switch, separated by space: ").split()))
+  a, b = order
+  temp = series[a]
+  series[a] = series[b]
+  series[b] = temp
+
+if check(serise):
+  print(num_required)
+else:
+  print(-1)
+
+#9.2
+
 
   
-
+    
